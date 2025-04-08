75d0135ea84eab3b11cea63d36bcad60b14b3d4f
Adedayo UWensuyi <onehpee@yahoo.com>
2025-04-07 21:46:52 -0500

    venv

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for, session
+import secrets
 
 def create_app():
     app = Flask(__name__)
@@ -6,6 +7,17 @@ def create_app():
     
     from .views import views
     from .auth import auth
+    
+    def converter_unit(units, value, unit_from, unit_to):
+        # same unit
+        if unit_from == unit_to:
+            return value
+        # converter to refer_unit
+        refer_unit = float(value) * units[unit_from]
+
+        # converter to desired
+        return refer_unit / units[unit_to]
+
     
     app.register_blueprint(views, url_prefix='/' )
     app.register_blueprint(auth, url_prefix='/' )
