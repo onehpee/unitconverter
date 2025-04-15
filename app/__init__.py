0eb6ad2a797452ca7e092de5016e5a2f496d01b4
Adedayo UWensuyi <onehpee@yahoo.com>
2025-04-14 23:51:50 -0500

    venv

--- app/__init__.py
+++ app/__init__.py
@@ -18,6 +18,28 @@ def create_app():
         # converter to desired
         return refer_unit / units[unit_to]
     
+    def convert_temp(value, unit_from, unit_to):
+        value = float(value)
+        if unit_from == unit_to:
+            return value
+
+        #original unit to Celsius
+        if unit_from == "c":
+            value_in_celsius = value
+        elif unit_from == "f":
+            value_in_celsius = (value - 32) * 5 / 9
+        else:
+            value_in_celsius = value - 273.15
+
+        #Celsius to the target unit
+        if unit_to == "c":
+            return value_in_celsius
+        elif unit_to == "f":
+            return (value_in_celsius * 9 / 5) + 32
+        else:
+            return value_in_celsius + 273.15
+
+    
     def length():
     # key unit value in meter
         LENGTH = {
@@ -30,6 +52,7 @@ def create_app():
         "yard": 0.9144,
         "mile": 1609.344,
     }
+        
 
     
     app.register_blueprint(views, url_prefix='/' )
