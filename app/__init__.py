db662b54931bf1617fae8ce2cd10f503760e6102
Adedayo UWensuyi <onehpee@yahoo.com>
2025-05-13 23:32:10 -0500

    smaller function

--- app/__init__.py
+++ app/__init__.py
@@ -1,18 +1,35 @@
-import tkinter as tk
+from flask import Flask, render_template,request
 
-def convert():
-    input_value = float(input_entry.get())
-    from_unit = from_unit_var.get()
-    to_unit = to_unit_var.get()
-    
-    
-    conversion_rates = {
-        ("Miles", "Kilometer"): 1.60934,
-        ("Kilometer", "Miles"): 0.621371,
-        ("Pounds", "Kilograms"): 0.453592,
-        ("Kilogrmas", "Pounds"): 2.20462,
-        ("Inches", "Centimeters"): 2.54,
-        ("Centimeters", "Inches"): 0.394
-    }
-    
-    result = input_value * conversion_rates[]
+app= Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('length.html')
+
+
+def length():
+    if request.method == 'POST':
+        try:
+            length = float(request.form["len"])
+            unit_from = request.form["unit_from"]
+            unit_to = request.form["unit_to"]
+            conversion_factors = {
+                'cm': 1,
+                'm': 100,
+                'km': 100000,
+                'in': 2.54,
+                'ft': 30.48,
+                'yd': 91.44,
+                'mi': 160934
+             }
+            if unit_from in conversion_factors and unit_to in conversion_factors:
+                converted = length * (conversion_factors[unit_from] / conversion_factors[unit_to])
+            else:
+                error = "Invalid unit conversion."
+                return render_template('result.html', error=error)
+        except ValueError:
+            error= "Invalid input. Please enter a valid number."
+            return render_template('result.html', error=error)
+        
+        return render_template('result.html', converted=converted, unit_to=unit_to)
+    return render_template('length.html')
